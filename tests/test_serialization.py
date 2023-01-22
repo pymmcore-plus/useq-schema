d988f5de5be95be60d0cfa5b95c1133e3b8c3cfd
Talley Lambert <talley.lambert@gmail.com>
2023-01-22 15:43:57 -0500

    test: update serdes tests (#71)

---
+++ tests/test_serialization.py
@@ -0,0 +1,18 @@
+import json
+from pathlib import Path
+
+import pytest
+
+from useq import MDASequence
+
+
+@pytest.mark.parametrize("ext", ["json", "yaml"])
+def test_serialization(mda1: MDASequence, ext: str) -> None:
+    FILE = Path(__file__).parent / "fixtures" / f"mda.{ext}"
+    text = FILE.read_text()
+    mda = MDASequence.parse_file(str(FILE))
+    assert mda == mda1
+    if ext == "json":
+        assert json.loads(mda.json(exclude={"uid"})) == json.loads(text)
+    else:
+        assert mda.yaml() == text
