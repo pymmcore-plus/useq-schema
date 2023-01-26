31e80a563fe46e21f8ef33604cde247aefcfe887
fdrgsp <federico.gasparoli@gmail.com>
2023-01-25 20:21:33 -0500

    feat: OrderMode

--- src/useq/_tile.py
+++ src/useq/_tile.py
@@ -12,6 +12,40 @@ class RelativeTo(Enum):
     center = "center"
     top_left = "top_left"
 
+class OrderMode(Enum):
+    row_wise = "row_wise"
+    column_wise = "column_wise"
+    snake_row_wise = "snake_row_wise"
+    snake_column_wise = "snake_column_wise"
+    spiral_in = "spiral_in"
+    spiral_out = "spiral_out"
+
+
+def _spiral_indices(rows: int, columns: int) -> Iterator[tuple[int, int]]:
+    """Return a spiral iterator over a 2D grid.
+
+    Parameters
+    ----------
+    rows : int
+        Number of rows.
+    columns : int
+        Number of columns.
+
+    Yields
+    ------
+    (x, y) : tuple[int, int]
+        Indices of the next element in the spiral.
+    """
+    x = y = 0
+    dx = 0
+    dy = -1
+    for _ in range(max(columns, rows) ** 2):
+        if (-columns / 2 < x <= columns / 2) and (-rows / 2 < y <= rows / 2):
+            yield x, y
+        if x == y or (x < 0 and x == -y) or (x > 0 and x == 1 - y):
+            dx, dy = -dy, dx
+        x, y = x + dx, y + dy
+
 
 class Coordinate(FrozenModel):
     """Defines a position in 2D space.
@@ -61,7 +95,7 @@ class _TilePlan(FrozenModel):
     """
 
     overlap: Tuple[float, float] = (0.0, 0.0)
-    snake_order: bool = True
+    order_mode: OrderMode = OrderMode.snake_row_wise
 
     @validator("overlap", pre=True)
     def _validate_overlap(cls, v: Any) -> Tuple[float, float]:
@@ -96,10 +130,35 @@ class _TilePlan(FrozenModel):
         cols = self._ncols(dy)
         x0 = self._offset_x(dx)
         y0 = self._offset_y(dy)
-        for r, c in itertools.product(range(rows), range(cols)):
-            if self.snake_order and r % 2 == 1:
-                c = cols - c - 1
-            yield TilePosition(x0 + c * dx, y0 - r * dy, r, c, self.is_relative)
+
+
+        if self.order_mode == OrderMode.row_wise:
+            for r, c in itertools.product(range(rows), range(cols)):
+                yield TilePosition(x0 + c * dx, y0 - r * dy, r, c, self.is_relative)
+
+        if self.order_mode == OrderMode.snake_row_wise:
+            for r, c in itertools.product(range(rows), range(cols)):
+                if r % 2 == 1:
+                    c = cols - c - 1
+                yield TilePosition(x0 + c * dx, y0 - r * dy, r, c, self.is_relative)
+        # if self.order_mode in {OrderMode.row_wise, OrderMode.snake_row_wise}:
+        #     for r, c in itertools.product(range(rows), range(cols)):
+        #         if self.order_mode == OrderMode.snake_row_wise and r % 2 == 1:
+        #             c = cols - c - 1
+        #         yield TilePosition(x0 + c * dx, y0 - r * dy, r, c, self.is_relative)
+        
+        elif self.order_mode in {OrderMode.column_wise, OrderMode.snake_column_wise}:
+            for c, r in itertools.product(range(cols), range(rows)):
+                if self.order_mode == OrderMode.snake_column_wise and c % 2 == 1:
+                    r = rows - r - 1
+                yield TilePosition(x0 + c * dx, y0 - r * dy, r, c, self.is_relative)
+        
+        elif self.order_mode in {OrderMode.spiral_in, OrderMode.spiral_out}:
+            for r, c in list(_spiral_indices(rows, cols)):
+                # yield TilePosition(x0 + c * dx, y0 - r * dy, r, c, self.is_relative)
+                yield TilePosition(x0 + c * dx, y0 + r * dy, r, c, self.is_relative)
+                # yield TilePosition(x0 - c * dx, y0 + r * dy, r, c, self.is_relative)
+                # yield TilePosition(x0 - c * dx, y0 - r * dy, r, c, self.is_relative)
 
     def __len__(self) -> int:
         return len(list(self.iter_tiles(1, 1)))
