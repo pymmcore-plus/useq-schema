e558c1eec0ddb7e3ff43c3ebd8be19373ecab689
Talley Lambert <talley.lambert@gmail.com>
2023-01-30 15:58:22 -0500

    tests: update tests and change index generation

--- src/useq/_tile.py
+++ src/useq/_tile.py
@@ -1,9 +1,11 @@
-import itertools
+from functools import partial
+import numpy as np
 import math
 from enum import Enum
-from typing import Any, Iterator, NamedTuple, Sequence, Tuple, Union
+from typing import Any, Callable, Iterator, NamedTuple, Sequence, Tuple, Union
 
 from pydantic import validator
+
 
 from useq._base_model import FrozenModel
 
@@ -18,12 +20,14 @@ class OrderMode(Enum):
 
     row_wise = "row_wise"
     column_wise = "column_wise"
-    snake_row_wise = "snake_row_wise"
-    snake_column_wise = "snake_column_wise"
+    row_wise_snake = "row_wise_snake"
+    column_wise_snake = "column_wise_snake"
     spiral = "spiral"
 
 
-def _spiral_indices(rows: int, columns: int) -> Iterator[Tuple[int, int]]:
+def _spiral_indices(
+    rows: int, columns: int, center_origin: bool = False
+) -> Iterator[Tuple[int, int]]:
     """Return a spiral iterator over a 2D grid.
 
     Parameters
@@ -32,21 +36,53 @@ def _spiral_indices(rows: int, columns: int) -> Iterator[Tuple[int, int]]:
         Number of rows.
     columns : int
         Number of columns.
+    center_origin : bool
+        If center_origin is True, all indices are centered around (0, 0), and some will
+        be negative. Otherwise, the indices are centered around (rows//2, columns//2)
 
     Yields
     ------
     (x, y) : tuple[int, int]
         Indices of the next element in the spiral.
     """
+    # direction: first down and then clockwise (assuming positive Y is down)
+
     x = y = 0
+    if center_origin:  # see docstring
+        xshift = yshift = 0
+    else:
+        xshift = (columns - 1) // 2
+        yshift = (rows - 1) // 2
     dx = 0
     dy = -1
     for _ in range(max(columns, rows) ** 2):
         if (-columns / 2 < x <= columns / 2) and (-rows / 2 < y <= rows / 2):
-            yield x, y
+            yield y + yshift, x + xshift
         if x == y or (x < 0 and x == -y) or (x > 0 and x == 1 - y):
             dx, dy = -dy, dx
         x, y = x + dx, y + dy
+
+
+# function that iterates indices (row, col) in a grid where (0, 0) is the top left
+def _rect_indices(
+    rows: int, columns: int, snake: bool = False, row_wise: bool = True
+) -> Iterator[Tuple[int, int]]:
+    c, r = np.meshgrid(np.arange(columns), np.arange(rows))
+    if snake:
+        if row_wise:
+            c[1::2, :] = c[1::2, :][:, ::-1]
+        else:
+            r[:, 1::2] = r[:, 1::2][::-1, :]
+    return zip(r.ravel(), c.ravel()) if row_wise else zip(r.T.ravel(), c.T.ravel())
+
+
+_INDEX_GENERATORS: dict[OrderMode, Callable] = {
+    OrderMode.row_wise: partial(_rect_indices, snake=False, row_wise=True),
+    OrderMode.column_wise: partial(_rect_indices, snake=False, row_wise=False),
+    OrderMode.row_wise_snake: partial(_rect_indices, snake=True, row_wise=True),
+    OrderMode.column_wise_snake: partial(_rect_indices, snake=True, row_wise=False),
+    OrderMode.spiral: _spiral_indices,
+}
 
 
 class Coordinate(FrozenModel):
@@ -65,13 +101,13 @@ class Coordinate(FrozenModel):
 
     @classmethod
     def validate(cls, v: Any) -> "Coordinate":
-        if isinstance(v, Coordinate):
+        if isinstance(v, Coordinate):  # pragma: no cover
             return v
         if isinstance(v, dict):
             return Coordinate(**v)
         if isinstance(v, (list, tuple)):
             return Coordinate(x=v[0], y=v[1])
-        raise ValueError(f"Cannot convert to Coordinate: {v}")
+        raise ValueError(f"Cannot convert to Coordinate: {v}")  # pragma: no cover
 
 
 class TilePosition(NamedTuple):
@@ -93,12 +129,12 @@ class _TilePlan(FrozenModel):
         for x and the second for y.
     order_mode : OrderMode
         Define the ways of ordering the grid positions. Options are
-        row_wise, column_wise, snake_row_wise, snake_column_wise and spiral.
-        By default, snake_row_wise.
+        row_wise, column_wise, row_wise_snake, column_wise_snake and spiral.
+        By default, row_wise_snake.
     """
 
     overlap: Tuple[float, float] = (0.0, 0.0)
-    order_mode: OrderMode = OrderMode.snake_row_wise
+    order_mode: OrderMode = OrderMode.row_wise_snake
 
     @validator("overlap", pre=True)
     def _validate_overlap(cls, v: Any) -> Tuple[float, float]:
@@ -106,50 +142,37 @@ class _TilePlan(FrozenModel):
             return (v,) * 2
         if isinstance(v, Sequence) and len(v) == 2:
             return float(v[0]), float(v[1])
-        raise ValueError("overlap must be a float or a tuple of two floats")
+        raise ValueError(  # pragma: no cover
+            "overlap must be a float or a tuple of two floats"
+        )
 
     @property
     def is_relative(self) -> bool:
         return False
 
     def _offset_x(self, dx: float) -> float:
-        return 0
+        raise NotImplementedError
 
     def _offset_y(self, dy: float) -> float:
-        return 0
+        raise NotImplementedError
 
     def _nrows(self, dx: float) -> int:
         """Return the number of rows, given a grid step size."""
         raise NotImplementedError
 
-    def _ncols(self, dy: float) -> int:
-        """Return the number of cols, given a grid step size."""
+    def _ncolumns(self, dy: float) -> int:
+        """Return the number of columns, given a grid step size."""
         raise NotImplementedError
 
     def iter_tiles(self, fov_width: float, fov_height: float) -> Iterator[TilePosition]:
         """Iterate over all tiles, given a field of view size."""
         dx, dy = self._step_size(fov_width, fov_height)
         rows = self._nrows(dx)
-        cols = self._ncols(dy)
+        cols = self._ncolumns(dy)
         x0 = self._offset_x(dx)
         y0 = self._offset_y(dy)
-
-        if self.order_mode in {OrderMode.row_wise, OrderMode.snake_row_wise}:
-            for r, c in itertools.product(range(rows), range(cols)):
-                if self.order_mode == OrderMode.snake_row_wise and r % 2 == 1:
-                    c = cols - c - 1
-                yield TilePosition(x0 + c * dx, y0 - r * dy, r, c, self.is_relative)
-
-        elif self.order_mode in {OrderMode.column_wise, OrderMode.snake_column_wise}:
-            for c, r in itertools.product(range(cols), range(rows)):
-                if self.order_mode == OrderMode.snake_column_wise and c % 2 == 1:
-                    r = rows - r - 1
-                yield TilePosition(x0 + c * dx, y0 - r * dy, r, c, self.is_relative)
-
-        elif self.order_mode == OrderMode.spiral:
-            for r, c in list(_spiral_indices(rows, cols)):
-                # direction: first up and then clockwise
-                yield TilePosition(x0 + c * dx, y0 + r * dy, r, c, self.is_relative)
+        for r, c in _INDEX_GENERATORS[self.order_mode](rows, cols):
+            yield TilePosition(x0 + c * dx, y0 - r * dy, r, c, self.is_relative)
 
     def __len__(self) -> int:
         return len(list(self.iter_tiles(1, 1)))
@@ -180,20 +203,14 @@ class TileFromCorners(_TilePlan):
         total_width = abs(self.corner1.x - self.corner2.x) + dx
         return math.ceil(total_width / dx)
 
-    def _ncols(self, dy: float) -> int:
+    def _ncolumns(self, dy: float) -> int:
         total_height = abs(self.corner1.y - self.corner2.y) + dy
         return math.ceil(total_height / dy)
 
     def _offset_x(self, dx: float) -> float:
-        if self.order_mode != OrderMode.spiral:
-            # if spiral, start from the center between corner1 and corner2
-            return min(self.corner1.x, self.corner2.x)
         return abs(self.corner1.x - self.corner2.x) / 2
 
     def _offset_y(self, dy: float) -> float:
-        if self.order_mode != OrderMode.spiral:
-            # if spiral, start from the center between corner1 and corner2
-            return min(self.corner1.y, self.corner2.y)
         return abs(self.corner1.y - self.corner2.y) / 2
 
 
@@ -204,7 +221,7 @@ class TileRelative(_TilePlan):
     ----------
     rows: int
         Number of rows.
-    cols: int
+    columns: int
         Number of columns.
     relative_to : RelativeTo
         Point in the grid to which the coordinates are relative. If "center", the grid
@@ -213,7 +230,7 @@ class TileRelative(_TilePlan):
     """
 
     rows: int
-    cols: int
+    columns: int
     relative_to: RelativeTo = RelativeTo.center
 
     @property
@@ -223,21 +240,17 @@ class TileRelative(_TilePlan):
     def _nrows(self, dx: float) -> int:
         return self.rows
 
-    def _ncols(self, dy: float) -> int:
-        return self.cols
+    def _ncolumns(self, dy: float) -> int:
+        return self.columns
 
     def _offset_x(self, dx: float) -> float:
-        if self.order_mode == OrderMode.spiral:
-            return 0.0
         return (
-            -((self.cols - 1) * dx) / 2
+            -((self.columns - 1) * dx) / 2
             if self.relative_to == RelativeTo.center
             else 0.0
         )
 
     def _offset_y(self, dy: float) -> float:
-        if self.order_mode == OrderMode.spiral:
-            return 0.0
         return (
             ((self.rows - 1) * dy) / 2 if self.relative_to == RelativeTo.center else 0.0
         )
--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -76,40 +76,40 @@ t_inputs = t_as_class + t_as_dict
 
 g_as_dict = [
     (
-        {"overlap": 10.0, "rows": 1, "cols": 2, "relative_to": "center"},
-        [(10.0, 10.0), OrderMode.snake_row_wise, 1, 2, RelativeTo.center],
+        {"overlap": 10.0, "rows": 1, "columns": 2, "relative_to": "center"},
+        [(10.0, 10.0), OrderMode.row_wise_snake, 1, 2, RelativeTo.center],
     ),
     (
-        {"overlap": 10.0, "rows": 1, "cols": 2, "relative_to": "top_left"},
-        [(10.0, 10.0), OrderMode.snake_row_wise, 1, 2, RelativeTo.top_left],
+        {"overlap": 10.0, "rows": 1, "columns": 2, "relative_to": "top_left"},
+        [(10.0, 10.0), OrderMode.row_wise_snake, 1, 2, RelativeTo.top_left],
     ),
     (
         {"overlap": 10.0, "corner1": {"x": 0, "y": 0}, "corner2": {"x": 2, "y": 2}},
         [
             (10.0, 10.0),
-            OrderMode.snake_row_wise,
+            OrderMode.row_wise_snake,
             Coordinate(x=0, y=0),
             Coordinate(x=2, y=2),
         ],
     ),
-    ({}, [(0.0, 0.0), OrderMode.snake_row_wise]),
+    ({}, [(0.0, 0.0), OrderMode.row_wise_snake]),
 ]
 
 g_as_class = [
     (
-        TileRelative(overlap=10.0, rows=1, cols=2, relative_to="center"),
-        [(10.0, 10.0), OrderMode.snake_row_wise, 1, 2, RelativeTo.center],
+        TileRelative(overlap=10.0, rows=1, columns=2, relative_to="center"),
+        [(10.0, 10.0), OrderMode.row_wise_snake, 1, 2, RelativeTo.center],
     ),
     (
         TileFromCorners(overlap=10.0, corner1=(0, 0), corner2=(2, 2)),
         [
             (10.0, 10.0),
-            OrderMode.snake_row_wise,
+            OrderMode.row_wise_snake,
             Coordinate(x=0, y=0),
             Coordinate(x=2, y=2),
         ],
     ),
-    (NoTile(), [(0.0, 0.0), OrderMode.snake_row_wise]),
+    (NoTile(), [(0.0, 0.0), OrderMode.row_wise_snake]),
 ]
 g_inputs = g_as_class + g_as_dict
 
