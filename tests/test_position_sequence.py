9c814b64763116bf3c4902942f881c532a83dbf8
fdrgsp <federico.gasparoli@gmail.com>
2023-02-27 21:17:21 -0500

    test: update test

--- tests/test_position_sequence.py
+++ tests/test_position_sequence.py
@@ -205,6 +205,29 @@ def test_grid_relative_in_main_and_grid_absolute_in_position_sub_sequence():
     ]
 
 
+def test_multi_g_in_position_sub_sequence():
+    mda = MDASequence(
+        stage_positions=[
+            {"sequence": {"grid_plan": {"rows": 1, "columns": 2}}},
+            {"sequence": {"grid_plan": {"rows": 2, "columns": 2}}},
+            {"sequence": {"grid_plan": {"top": 1, "bottom": -1, "left": 0, "right": 0}}}
+        ]
+    )
+    assert [
+        (i.global_index, i.index, i.x_pos, i.y_pos) for i in mda.iter_events()
+    ] == [
+        (0, {"p": 0, "g": 0}, -0.5, 0.0),
+        (1, {"p": 0, "g": 1}, 0.5, 0.0),
+        (2, {"p": 1, "g": 0}, -0.5, 0.5),
+        (3, {"p": 1, "g": 1}, 0.5, 0.5),
+        (4, {"p": 1, "g": 2}, 0.5, -0.5),
+        (5, {"p": 1, "g": 3}, -0.5, -0.5),
+        (6, {"p": 2, "g": 0}, 0.0, 1.0),
+        (7, {"p": 2, "g": 1}, 0.0, 0.0),
+        (8, {"p": 2, "g": 2}, 0.0, -1.0),
+    ]
+
+
 # test_z_plan
 def test_z_relative_with_multi_stage_positions():
     mda = MDASequence(
