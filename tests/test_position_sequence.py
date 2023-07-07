7486cf92667cf1c911814b1d1eb2acd55c69a62c
federico gasparoli <70725613+fdrgsp@users.noreply.github.com>
2023-07-07 15:36:01 -0400

    fix: MDASequence iter_sequence method (#93)
    * fix: fix iter_sequence logic if channel and any plan are specified
    
    * test: simplify

--- tests/test_position_sequence.py
+++ tests/test_position_sequence.py
@@ -676,3 +676,18 @@ def test_channels_and_pos_z_grid_and_time_plan():
 
     chs = {i.channel.config for i in mda}
     assert chs == {"Cy5", "FITC"}
+
+
+def test_sub_channels_and_any_plan():
+    # test that only specified sub-channels are acquired for each z plan
+    mda = MDASequence(
+        axis_order="tpgcz",
+        channels=["Cy5", "FITC"],
+        stage_positions=[
+            {"sequence": {"channels": ["DAPI"], "z_plan": {"range": 2, "step": 1}}}
+        ],
+    )
+
+    chs = {i.channel.config for i in mda}
+    assert chs == {"DAPI"}
+    assert len(list(mda.iter_events())) == 3
