8ad0da103d294c8179bc9f30b6a33fac4082a3a2
Talley Lambert <talley.lambert@gmail.com>
2023-08-12 14:01:42 -0400

    add exports

--- src/useq/__init__.py
+++ src/useq/__init__.py
@@ -1,23 +1,21 @@
+from typing import Any
+
 from useq._actions import AcquireImage, Action, HardwareAutofocus
 from useq._channel import Channel
-from useq._grid import (
-    AnyGridPlan,
-    GridFromEdges,
-    GridRelative,
-    GridRowsColumns,
-    GridWidthHeight,
-)
+from useq._grid import AnyGridPlan, GridFromEdges, GridRowsColumns, GridWidthHeight
 from useq._hardware_autofocus import AnyAutofocusPlan, AutoFocusPlan, AxesBasedAF
 from useq._mda_event import MDAEvent, PropertyTuple
 from useq._mda_sequence import MDASequence
 from useq._position import Position
 from useq._time import (
+    AnyTimePlan,
     MultiPhaseTimePlan,
     TDurationLoops,
     TIntervalDuration,
     TIntervalLoops,
 )
 from useq._z import (
+    AnyZPlan,
     ZAboveBelow,
     ZAbsolutePositions,
     ZRangeAround,
@@ -30,6 +28,8 @@ __all__ = [
     "Action",
     "AnyAutofocusPlan",
     "AnyGridPlan",
+    "AnyTimePlan",
+    "AnyZPlan",
     "AutoFocusPlan",
     "AxesBasedAF",
     "Channel",
@@ -57,3 +57,19 @@ __all__ = [
 # type ignores because pydantic-compat consumes the kwargs
 MDAEvent.model_rebuild(MDASequence=MDASequence)  # type: ignore  [call-arg]
 Position.model_rebuild(MDASequence=MDASequence)  # type: ignore  [call-arg]
+
+
+def __getattr__(name: str) -> Any:
+    if name == "GridRelative":
+        import warnings
+
+        from useq._grid import GridRowsColumns
+
+        warnings.warn(
+            "useq.GridRelative has been renamed to useq.GridFromEdges",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+
+        return GridRowsColumns
+    raise AttributeError(f"module {__name__} has no attribute {name}")
