56f3394a4c5538888b036338624a30601a7d4f8a
Talley Lambert <talley.lambert@gmail.com>
2023-08-16 12:45:13 -0400

    feat: add GridWidthHeight (#129)
    * feat: add GridWidthHeight
    
    * add exports
    
    * don't warn
    
    * docs: fix docs
    
    * add test

--- src/useq/__init__.py
+++ src/useq/__init__.py
@@ -1,17 +1,21 @@
+from typing import Any
+
 from useq._actions import AcquireImage, Action, HardwareAutofocus
 from useq._channel import Channel
-from useq._grid import AnyGridPlan, GridFromEdges, GridRelative
+from useq._grid import AnyGridPlan, GridFromEdges, GridRowsColumns, GridWidthHeight
 from useq._hardware_autofocus import AnyAutofocusPlan, AutoFocusPlan, AxesBasedAF
 from useq._mda_event import MDAEvent, PropertyTuple
 from useq._mda_sequence import MDASequence
 from useq._position import Position
 from useq._time import (
+    AnyTimePlan,
     MultiPhaseTimePlan,
     TDurationLoops,
     TIntervalDuration,
     TIntervalLoops,
 )
 from useq._z import (
+    AnyZPlan,
     ZAboveBelow,
     ZAbsolutePositions,
     ZRangeAround,
@@ -24,11 +28,15 @@ __all__ = [
     "Action",
     "AnyAutofocusPlan",
     "AnyGridPlan",
+    "AnyTimePlan",
+    "AnyZPlan",
     "AutoFocusPlan",
     "AxesBasedAF",
     "Channel",
     "GridFromEdges",
     "GridRelative",
+    "GridRowsColumns",
+    "GridWidthHeight",
     "HardwareAutofocus",
     "MDAEvent",
     "MDASequence",
@@ -49,3 +57,17 @@ __all__ = [
 # type ignores because pydantic-compat consumes the kwargs
 MDAEvent.model_rebuild(MDASequence=MDASequence)  # type: ignore  [call-arg]
 Position.model_rebuild(MDASequence=MDASequence)  # type: ignore  [call-arg]
+
+
+def __getattr__(name: str) -> Any:
+    if name == "GridRelative":
+        from useq._grid import GridRowsColumns
+
+        # warnings.warn(
+        #     "useq.GridRelative has been renamed to useq.GridFromEdges",
+        #     DeprecationWarning,
+        #     stacklevel=2,
+        # )
+
+        return GridRowsColumns
+    raise AttributeError(f"module {__name__} has no attribute {name}")
