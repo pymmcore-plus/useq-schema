1847b56cabaeb28f5dfe2101bf0e5fb515869358
Talley Lambert <talley.lambert@gmail.com>
2024-10-27 18:00:54 +0100

    add should_skip and  create_event_kwargs patterns

--- example.py
+++ example.py
@@ -1,7 +1,10 @@
 import abc
+import sys
 from dataclasses import dataclass
 from itertools import count, islice, product
-from typing import Iterable, Iterator, TypeVar, cast
+from typing import Iterable, Iterator, Sequence, TypeVar, cast
+
+from useq._mda_event import MDAEvent
 
 T = TypeVar("T")
 
@@ -21,21 +24,34 @@ class AxisIterator(Iterable[T]):
         """
         return self.INFINITE
 
+    @abc.abstractmethod
+    def create_event_kwargs(cls, val: T) -> dict: ...
+
+    @classmethod
+    def should_skip(cls, kwargs: dict) -> bool:
+        return False
+
 
 class TimePlan(AxisIterator[float]):
+    def __init__(self, tpoints: Sequence[float]) -> None:
+        self._tpoints = tpoints
+
     axis_key = "t"
 
     def __iter__(self) -> Iterator[float]:
-        yield 1.0
-        yield 2.0
+        yield from self._tpoints
 
     def length(self) -> int:
-        return 2
+        return len(self._tpoints)
+
+    def create_event_kwargs(cls, val: float) -> dict:
+        return {"min_start_time": val}
 
 
 class ZPlan(AxisIterator[int]):
     def __init__(self, stop: int | None = None) -> None:
         self._stop = stop
+        self.acquire_every = 2
 
     axis_key = "z"
 
@@ -46,6 +62,15 @@ class ZPlan(AxisIterator[int]):
 
     def length(self) -> int:
         return self._stop or self.INFINITE
+
+    def create_event_kwargs(cls, val: int) -> dict:
+        return {"z_pos": val}
+
+    def should_skip(self, event: dict) -> bool:
+        index = event["index"]
+        if "t" in index and index["t"] % self.acquire_every:
+            return True
+        return False
 
 
 @dataclass
@@ -66,7 +91,19 @@ class MySequence:
         for idx, val in enumerate(ax, start):
             yield key, idx, val
 
-    def __iter__(self) -> Iterator[tuple[str, T]]:
+    def __iter__(self) -> MDAEvent:
+        ax_map: dict[str, type[AxisIterator]] = {ax.axis_key: ax for ax in self.axes}
+        for item in self._iter_inner():
+            event: dict = {"index": {}}
+            for axis_key, index, value in item:
+                ax_type = ax_map[axis_key]
+                event["index"][axis_key] = index
+                event.update(ax_type.create_event_kwargs(value))
+
+            if not any(ax_type.should_skip(event) for ax_type in ax_map.values()):
+                yield MDAEvent(**event)
+
+    def _iter_inner(self) -> Iterator[tuple[str, int, T]]:
         """Iterate over the sequence."""
         ax_map = {ax.axis_key: ax for ax in self.axes}
         sorted_axes = [ax_map[key] for key in self.order]
@@ -93,3 +130,12 @@ class MySequence:
             iterators.append(self._enumerate_ax(ax.axis_key, iterator, begin))
 
         return product(*iterators)
+
+
+if __name__ == "__main__":
+    seq = MySequence(axes=(TimePlan((0, 1, 2, 3, 4)), ZPlan(3)), order=("t", "z"))
+    if seq.is_infinite:
+        print("Infinite sequence")
+        sys.exit(0)
+    for event in seq:
+        print(event)
